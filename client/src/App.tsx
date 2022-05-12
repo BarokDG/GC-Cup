ffa552d054201bc7e03c3a41f3cac131222e4424
BarokDG <it.barok.dagim@gmail.com>
2022-05-12 21:19:16 +0300

    Add views, conditional rendering logic

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,21 @@
+import { useState, useEffect } from "react";
+import DesktopView from "./pages/desktopView";
+import MobileView from "./pages/mobileView";
+
 function App() {
-  return <div className="App"></div>;
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    window.addEventListener("resize", () => setWindowWidth(window.innerWidth));
+
+    return function cleanup() {
+      window.removeEventListener("resize", () =>
+        setWindowWidth(window.innerWidth)
+      );
+    };
+  }, []);
+
+  return <>{windowWidth > 700 ? <DesktopView /> : <MobileView />}</>;
 }
 
 export default App;
---
+++ client/src/pages/mobileView.tsx
@@ -0,0 +1,3 @@
+export default function MobileView() {
+  return <h1>Mobile</h1>;
+}

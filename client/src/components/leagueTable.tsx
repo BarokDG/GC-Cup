88a4f28b095f9dc0bac41b934e97e8179ba11013
BarokDG <it.barok.dagim@gmail.com>
2022-05-14 00:01:46 +0300

    Add separate tables for conferences

--- client/src/components/leagueTable.tsx
+++ client/src/components/leagueTable.tsx
@@ -1,66 +1,44 @@
-import { teams } from "../utils/data-placeholder/team.data";
+import { useState, useEffect } from "react";
 
+import TableBody from "./tableBody";
 import "../css/table.css";
 
 export default function LeagueTable() {
+  const [conference, setConference] = useState(1);
+
+  useEffect(() => {
+    const conferenceSwitch: HTMLDivElement | null =
+      document.querySelector(".conference-switch");
+
+    conference === 2
+      ? conferenceSwitch?.style.setProperty("--start", "50%")
+      : conferenceSwitch?.style.setProperty("--start", "0");
+  }, [conference]);
+
   return (
-    <div className="table-container max-w-full overflow-auto">
-      <table className="border-collapse">
-        <thead>
-          <tr>
-            <th></th>
-            <th title="Matches Played">MP</th>
-            <th title="Points">Pts</th>
-            <th title="Wins">W</th>
-            <th title="Draws">D</th>
-            <th title="Losses">L</th>
-            <th>GF : GA</th>
-            <th title="Goal Difference">GD</th>
-            <th>Last 5</th>
-          </tr>
-        </thead>
-        <tbody>
-          {teams.map(
-            (
-              {
-                teamID,
-                teamName,
-                points,
-                played,
-                wins,
-                draws,
-                losses,
-                goalsFor,
-                goalsAgainst,
-                goalDifference,
-                lastFive,
-              },
-              index
-            ) => {
-              return (
-                <tr key={teamID}>
-                  <td>
-                    <div className="team-name">
-                      <span>{index + 1}</span>
-                      <span>{teamName}</span>
-                    </div>
-                  </td>
-                  <td>{played}</td>
-                  <td>{points}</td>
-                  <td>{wins}</td>
-                  <td>{draws}</td>
-                  <td>{losses}</td>
-                  <td>
-                    {goalsFor} : {goalsAgainst}
-                  </td>
-                  <td>{goalDifference}</td>
-                  <td>{lastFive}</td>
-                </tr>
-              );
-            }
-          )}
-        </tbody>
-      </table>
-    </div>
+    <>
+      <div className="conference-switch flex py-2 relative w-max">
+        <button onClick={() => setConference(1)}>Conference 1</button>
+        <button onClick={() => setConference(2)}>Conference 2</button>
+      </div>
+      <div className="table-container max-w-full overflow-auto">
+        <table className="border-collapse">
+          <thead>
+            <tr>
+              <th></th>
+              <th title="Matches Played">MP</th>
+              <th title="Points">Pts</th>
+              <th title="Wins">W</th>
+              <th title="Draws">D</th>
+              <th title="Losses">L</th>
+              <th>GF : GA</th>
+              <th title="Goal Difference">GD</th>
+              <th>Form</th>
+            </tr>
+          </thead>
+          <TableBody Conference={conference} />
+        </table>
+      </div>
+    </>
   );
 }

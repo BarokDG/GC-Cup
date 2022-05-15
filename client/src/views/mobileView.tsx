57066fefadba23948814d2084687bdf68839de4e
BarokDG <it.barok.dagim@gmail.com>
2022-05-15 14:09:47 +0300

    Add navbar

--- client/src/views/mobileView.tsx
+++ client/src/views/mobileView.tsx
@@ -2,9 +2,9 @@ import Navbar from "../components/navbar";
 import Header from "../components/header";
 
 import Table from "../components/table";
-import Fixtures from "../components/fixtures";
+import Matches from "../components/matches";
 import Stats from "../components/stats";
-import Teams from "../components/teams";
+import About from "../components/about";
 
 import { Routes, Route } from "react-router-dom";
 
@@ -12,12 +12,12 @@ export default function MobileView() {
   return (
     <>
       <Header />
-      <main className="px-2">
+      <main className="flex-grow bg-slate-900 px-2">
         <Routes>
           <Route index element={<Table />} />
-          <Route path="/fixtures" element={<Fixtures />} />
+          <Route path="/matches" element={<Matches />} />
           <Route path="/stats" element={<Stats />} />
-          <Route path="/teams" element={<Teams />} />
+          <Route path="/about" element={<About />} />
         </Routes>
       </main>
       <Navbar />

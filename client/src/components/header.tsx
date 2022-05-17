e5d7e49c18e10e20292a232c0695dd30322c1a04
BarokDG <it.barok.dagim@gmail.com>
2022-05-17 04:30:41 +0300

    Minor style changes

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <header className="bg-gray-800 py-2">
-      <h1 className="text-3xl text-center text-gray-200 font-bold">GC CUP</h1>
+      <h1 className="text-2xl text-center text-gray-300 font-bold">GC Cup</h1>
     </header>
   );
 }

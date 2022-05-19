3e5af7039616bc9caa10ec9b54cf1fcac586cfa5
BarokDG <it.barok.dagim@gmail.com>
2022-05-19 11:07:06 +0300

    Update desktopView.tsx

--- client/src/views/desktopView.tsx
+++ client/src/views/desktopView.tsx
@@ -4,10 +4,10 @@ export default function DesktopView() {
   return (
     <div className="h-screen grid place-items-center bg-slate-900">
       <div className="wrapper w-3/4">
-        <img src={Meme} alt="" />
+        <img className="mx-auto" src={Meme} alt="" />
         <p className="text-slate-300 text-center mt-6 text-lg">
-          Too lazy/buzy to redesign for larger screens at the moment. Maybe
-          later... Works great on Mobile tho, check it out!
+          Too lazy/buzy to redesign for larger screens at the moment, maybe
+          later. Works great on Mobile tho, check it out!
         </p>
       </div>
     </div>

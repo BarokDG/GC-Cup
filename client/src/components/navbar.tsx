c84881e15136f1d4eb7eb39736e86f29956835d9
BarokDG <it.barok.dagim@gmail.com>
2022-05-24 21:35:34 +0300

    Responsiveness

--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -9,7 +9,7 @@ import {
 
 export default function Navbar() {
   return (
-    <nav className="fixed bottom-0 w-full flex justify-between py-4 px-8 mb-2 bg-gray-800 rounded-full border border-b-4 border-gray-500">
+    <nav className="fixed bottom-0 w-full flex justify-between py-4 px-8 mb-2 bg-gray-800 rounded-full border border-b-4 border-gray-500 max-w-md left-1/2 -translate-x-1/2">
       <CustomNavLink to="/" icon={faList} text="Table" />
       <CustomNavLink to="/matches" icon={faDiceD6} text="Matches" />
       <CustomNavLink to="/stats" icon={faRankingStar} text="Stats" />

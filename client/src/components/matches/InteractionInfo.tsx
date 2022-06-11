8c66a94d7d7097c8d87577451fa944d7ec133960
BarokDG <it.barok.dagim@gmail.com>
2022-06-11 17:18:57 +0300

    Minor UI changes

--- client/src/components/matches/InteractionInfo.tsx
+++ client/src/components/matches/InteractionInfo.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 export default function InteractionInfo({ message }: Props) {
   return (
-    <div className="flex justify-center items-center mt-3">
+    <div className="flex justify-center items-center">
       <FontAwesomeIcon icon={faCircleInfo} className="text-slate-500 w-3 h-3" />
       <p className="ml-1 text-xs text-slate-300 leading-none">{message}</p>
     </div>

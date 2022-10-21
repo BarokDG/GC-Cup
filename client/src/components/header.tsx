d1a0bee04839fc237552609cbbeed2800ea76a1c
BarokDG <it.barok.dagim@gmail.com>
2022-10-21 11:02:43 +0300

    Remove meme from desktop view

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
   return (
     <header className="bg-gray-800 py-2 relative">
       <h1 className="text-2xl text-gray-300 font-bold flex justify-center items-center">
-        <span>GC</span> <GraduationCap className="w-8 h-8 inline-block mx-1" />{" "}
+        <span>GC</span> <GraduationCap className="w-8 h-8 inline-block mx-1" />
         <span>Cup</span>
       </h1>
       <MatchDayIndicator />

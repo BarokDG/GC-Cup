5ca6540d51a7752d43c3671bd9e1b3ee25808eaf
BarokDG <it.barok.dagim@gmail.com>
2022-10-21 11:42:12 +0300

    Add all components to desktop view

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -2,14 +2,18 @@ import MatchDayIndicator from "./matchDayIndicator";
 
 import { ReactComponent as GraduationCap } from "../assets/icons/graduation-cap.svg";
 
-export default function Header() {
+type Props = {
+  isMobile?: boolean;
+};
+
+export default function Header({ isMobile = true }: Props) {
   return (
     <header className="bg-gray-800 py-2 relative">
       <h1 className="text-2xl text-gray-300 font-bold flex justify-center items-center">
         <span>GC</span> <GraduationCap className="w-8 h-8 inline-block mx-1" />
         <span>Cup</span>
       </h1>
-      <MatchDayIndicator />
+      {isMobile && <MatchDayIndicator />}
     </header>
   );
 }
--- client/src/views/desktopView.tsx
+++ client/src/views/desktopView.tsx
@@ -1,3 +1,20 @@
+import Header from "../components/header";
+import Matches from "../components/matches";
+import Stats from "../components/stats";
+import LeagueTable from "../components/table/leagueTable";
+import PlayOffs from "../components/table/playoffs";
+
 export default function DesktopView() {
-  return <></>;
+  return (
+    <>
+      <div className="bg-slate-900 grid-container">
+        <Header isMobile={false} />
+        <LeagueTable conference={1} />
+        <LeagueTable conference={2} />
+        <PlayOffs conference={3} />
+        <Matches />
+        <Stats />
+      </div>
+    </>
+  );
 }
